13e6f17d17e6aae7523b9b4bd063252ddaf01ef8
Jaroslav Hejlek <hejlekjaroslav@gmail.com>
2018-03-29 12:48:21 +0200

    Allow SSL connection to mongodb (#7)
    * Attempts to tunnel ssl connections
    
    * Fixed host editation
    
    * Cleanup

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const Apify = require('apify');
 const Promise = require('bluebird');
+const fs = require('fs');
+// const dns = require('dns');
+const ping = require('ping');
+const hostile = require('hostile');
 const MongoClient = require('mongodb').MongoClient;
 const { createTunnel, closeTunnel } = require('proxy-chain');
 const _ = require('underscore');
@@ -40,12 +44,39 @@ Apify.main(async () => {
 
     let tunnels = null;
     if (input.proxyUrl) {
-        const match = mongoUrl.match(/mongodb:\/\/(.*)@([^/]*)\/?(.*)/);
+        let match = mongoUrl.match(/(mongodb):\/\/(.*)@([^/]*)\/?(.*)/); // v3.4 mongo string
+        if (!match) match = mongoUrl.match(/(mongodb\+srv):\/\/(.*)@([^/]*)\/?(.*)/); // v3.6 mongo string
         if (match) {
-            const [wholeString, credentials, host, additionalDetails] = match;
-            const hosts = host.split(',');
-            const tunnels = await Promise.map(hosts, (host) => createTunnel(input.proxyUrl, host));
-            mongoUrl = `mongodb://${credentials}@${tunnels.join(',')}${additionalDetails ? `/${additionalDetails}` : '' }`;
+            const [wholeString, protocol, credentials, host, additionalDetails] = match;
+            const pureHostnames = [];
+            const hosts = host.split(',').map(host => {
+                const [hostname, port] = host.split(':');
+                pureHostnames.push(hostname);
+                if (port == 2) return host;
+                else return `${host}:27017`; // default mongodb port is 27017 and is omited from basic 3.6 string
+            });
+
+            const tunnels = await Promise.map(hosts, (host) => createTunnel(input.proxyUrl, host))
+
+            if (!process.env.KEEP_HOSTS) {
+                // add item to /etc/hosts
+                await new Promise((resolve, reject) => {
+                    hostile.set('127.0.0.1', pureHostnames.join(' '), (err) => {
+                        if (err) return reject(err);
+                        return resolve();
+                    })
+                });
+
+                // Test connectivity to proxy
+                await Promise.all(pureHostnames.map(async (hostname) => {
+                    const data = await ping.promise.probe(hostname);
+                    console.log('Connecting to ip', data.numeric_host);
+                    console.log('Host is alive', data.alive);
+                }));
+            }
+
+            const transformedTunnels = tunnels.map((tunnel, i) => tunnel.replace('localhost', pureHostnames[i])).join(',');
+            mongoUrl = `${protocol}://${credentials}@${transformedTunnels}${additionalDetails ? `/${additionalDetails}` : '' }`;
         }
     }
 
@@ -54,6 +85,7 @@ Apify.main(async () => {
     console.log('Import to collection:', collectionName);
 
     const db = await MongoClient.connect(mongoUrl);
+
     const collection = await db.collection(collectionName);
 
     // Import
